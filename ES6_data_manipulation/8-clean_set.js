a3ed5cf0801e2fd338b4f63cb35ad1c7ea8e9b11
Ezio-33 <8691@holbertonstudents.com>
2024-07-23 10:50:49 +0200

    task 8, 9 et 10

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  const result = [...set]
+    .filter(
+      (value) => typeof value === 'string' && value.startsWith(startString),
+    )
+    .map((value) => value.slice(startString.length))
+    .join('-');
+
+  return result;
+}
+
+export default cleanSet;
