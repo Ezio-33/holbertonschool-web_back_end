028fe745511cbc81ee5d75e0790f3b839cc96556
Ezio-33 <8691@holbertonstudents.com>
2024-09-19 19:08:56 +0200

    ajout de commentaire

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,4 +1,18 @@
-const fs = require("fs").promises;
+// En utilisant la base de données database.csv (fournie dans la description du projet),
+// créez une fonction countStudents dans le fichier 3-read_file_async.js
+// créez une fonction nommée countStudents.
+// Elle doit accepter un chemin en argument (comme dans 2-read_file.js)
+// Le script doit tenter de lire le fichier de base de données de manière asynchrone
+// La fonction doit retourner une Promesse
+// Si la base de données n'est pas disponible, elle doit lancer une erreur avec le texte
+// "Cannot load the database"
+// Si la base de données est disponible, elle doit afficher le
+// message suivant dans la console "Number of students: NUMBER_OF_STUDENTS"
+// Elle doit afficher le nombre d'étudiants dans chaque domaine,
+// et la liste avec le format suivant : "Number of students in FIELD:
+// 6. List: LIST_OF_FIRSTNAMES"
+// Le fichier CSV peut contenir des lignes vides (à la fin) - et elles ne sont pas
+//des étudiants valides !const fs = require("fs").promises;
 
 async function countStudents(path) {
   try {
