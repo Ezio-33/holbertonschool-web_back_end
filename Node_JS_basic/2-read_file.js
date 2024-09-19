cdcdffb8bde8666f4b81bce9f8f0aa4479dac075
Ezio-33 <8691@holbertonstudents.com>
2024-09-19 17:12:57 +0200

    ajout de commentaire pour la task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,3 +1,15 @@
+// En utilisant la base de données database.csv (fournie dans la description du projet),
+// créez une fonction countStudents dans le fichier 2-read_file.js
+// Créez une fonction nommée countStudents. Elle doit accepter un chemin en argument.
+// Le script doit tenter de lire le fichier de base de données de manière synchrone.
+// Si la base de données n'est pas disponible, elle doit lancer une erreur avec le texte suivant :
+// "Impossible de charger la base de données"
+// Si la base de données est disponible, le script doit afficher le message suivant :
+// "Nombre d'étudiants : NUMBER_OF_STUDENTS"
+// Il doit enregistrer le nombre d'étudiants dans chaque domaine,
+// et la liste dans le format suivant : "Nombre d'étudiants en FIELD : 6. Liste : LIST_OF_FIRST_NAMES"
+// Le fichier CSV peut contenir des lignes vides (à la fin)
+
 const fs = require("fs");
 
 function countStudents(path) {
@@ -15,7 +27,7 @@ function countStudents(path) {
     const fields = {};
     for (const student of students.slice(1)) {
       if (student.length === 4) {
-        const [firstName, , , field] = student;
+        const [firstName, lastName, age, field] = student;
         if (fields[field]) {
           fields[field].push(firstName);
         } else {
